c2ec18cd929aad8070feaa9f8fd8fecd6609ad0f
DanielLarsson <daniel.larsson@2c8.com>
2025-09-04 10:07:30 +0200

    Add structure and db objects

---
+++ src/main/java/org/poolen/backend/db/maps/CharacterStore.java
@@ -0,0 +1,50 @@
+package org.poolen.backend.db.maps;
+
+import org.poolen.backend.db.constants.House;
+import org.poolen.backend.db.entities.Character;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class CharacterStore {
+
+    // The single, final instance of our class.
+    private static final CharacterStore INSTANCE = new CharacterStore();
+
+    private Map<UUID, Character> characterMap;
+
+    // Private constructor to prevent additional instances and to enforce
+    // singleton
+    private CharacterStore() {
+        this.characterMap = new HashMap<>();
+    }
+
+    public static CharacterStore getInstance() {
+        return INSTANCE;
+    }
+
+    public List<Character> getCharactersByHouse(House house) {
+        return this.characterMap.values().stream()
+                .filter(character -> character.getHouse().equals(house))
+                .collect(Collectors.toList());
+    }
+
+    public List<Character> getAllCharacters() {
+        return this.characterMap.values().stream().toList();
+    }
+
+    public Character getCharacterByUuid(UUID uuid) {
+        return this.characterMap.get(uuid);
+    }
+
+    public void addCharacter(Character character) {
+        this.characterMap.put(character.getUuid(), character);
+    }
+
+    public void addCharacter(List<Character> characters) {
+        characters.forEach(this::addCharacter);
+    }
+}
