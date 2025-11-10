df0182dd4c58aeb3b97b9cd66a3a7ca54ab4774c
DanielLarsson <daniel.larsson@2c8.com>
2025-11-10 11:41:00 +0100

    Fix h2 mode

--- matchmaker/src/main/java/org/poolen/web/email/EmailService.java
+++ matchmaker/src/main/java/org/poolen/web/email/EmailService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 
 @Service
+@Lazy
 public class EmailService {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
